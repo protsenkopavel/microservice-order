71a275c87c9a6cd79d9535281fc737ff90a7767c
Pavel Protsenko <pavelprotsenko@outlook.com>
2024-05-14 21:01:54 +0300

    project init

---
+++ src/test/java/net/protsenko/microserviceorder/ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.protsenko.microserviceorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
